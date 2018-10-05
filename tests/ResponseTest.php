78d145aff342b235af758463370d8ea170813a67
Kai Schwarz <kschwarz@hexonet.net>
2018-10-05 15:03:37 +0200

    refactor(tests): use of method chaining

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -12,8 +12,8 @@ final class ResponseTest extends TestCase
 
     public static function setupBeforeClass() {
         self::$rtm = RTM::getInstance();
-        self::$rtm->addTemplate("OK", self::$rtm->generateTemplate("200", "Command completed successfully"));
-        self::$rtm->addTemplate("listP0", "[RESPONSE]\r\nPROPERTY[TOTAL][0]=2701\r\nPROPERTY[FIRST][0]=0\r\nPROPERTY[DOMAIN][0]=0-60motorcycletimes.com\r\nPROPERTY[DOMAIN][1]=0-be-s01-0.com\r\nPROPERTY[COUNT][0]=2\r\nPROPERTY[LAST][0]=1\r\nPROPERTY[LIMIT][0]=2\r\nDESCRIPTION=Command completed successfully\r\nCODE=200\r\nQUEUETIME=0\r\nRUNTIME=0.023\r\nEOF\r\n");
+        self::$rtm->addTemplate("OK", self::$rtm->generateTemplate("200", "Command completed successfully"))
+                  ->addTemplate("listP0", "[RESPONSE]\r\nPROPERTY[TOTAL][0]=2701\r\nPROPERTY[FIRST][0]=0\r\nPROPERTY[DOMAIN][0]=0-60motorcycletimes.com\r\nPROPERTY[DOMAIN][1]=0-be-s01-0.com\r\nPROPERTY[COUNT][0]=2\r\nPROPERTY[LAST][0]=1\r\nPROPERTY[LIMIT][0]=2\r\nDESCRIPTION=Command completed successfully\r\nCODE=200\r\nQUEUETIME=0\r\nRUNTIME=0.023\r\nEOF\r\n");
     }
 
     public static function tearDownAfterClass() {
