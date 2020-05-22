da1f29ad6bb0f2a091f099ea0a5e35b708b8882f
Kai Schwarz <kschwarz@hexonet.net>
2020-05-22 14:01:10 +0200

    test(phpcompatibility): re-introduce `void` returntype CI/PHPUnit

--- tests/APIClientTest.php
+++ tests/APIClientTest.php
@@ -10,13 +10,13 @@ final class APIClientTest extends \PHPUnit\Framework\TestCase
 {
     public static $cl;
 
-    public static function setUpBeforeClass()
+    public static function setUpBeforeClass(): void
     {
         session_start();
         self::$cl = new CL();
     }
 
-    public static function tearDownAfterClass()
+    public static function tearDownAfterClass(): void
     {
         self::$cl = null;
         session_destroy();
