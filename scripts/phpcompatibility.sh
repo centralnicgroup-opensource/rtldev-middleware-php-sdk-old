958d1c4121fb665d08e1af1084ceeebc17c7d5d8
Kai Schwarz <kschwarz@hexonet.net>
2020-07-17 11:27:00 +0200

    ci(travis): fixed phpCompatibility check

--- scripts/phpcompatibility.sh
+++ scripts/phpcompatibility.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PHP_VERSION="$(php -r 'echo PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION;')"
-phpcs --ignore=registrars/ispapi/lib --standard=PHPCompatibility -q -n --colors --runtime-set testVersion "$PHP_VERSION" modules || exit 1;
+phpcs --ignore=registrars/ispapi/lib --standard=PHPCompatibility -q -n --colors --runtime-set testVersion "$PHP_VERSION" src || exit 1;
